d5e5b13e75895c189fade84751dc3983fe792cbc
Alexey Gluhih <tishalexey3@gmail.com>
2025-04-16 00:02:27 +0300

    add new feat

---
+++ hw14_javascript_1/script.js
@@ -0,0 +1,115 @@
+// Задача 1
+// let num = 1;
+// num += 12;
+// num -= 14;
+// num *= 5;
+// num /= 7;
+// num += 1;
+// num -= 1;
+// console.log(num); //-0.7142857142857143
+
+
+// Работа с переменными
+
+// let num = 3;
+// alert(num);
+
+// let a = 10;
+// let b = 2;
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
+
+// let c = 15;
+// let d = 2;
+// let result = c + d;
+// alert(result);
+
+// let a = 10;
+// let b = 2;
+// let c = 5;
+// alert(a + b + c);
+
+// let a = 17;
+// let b = 10;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+// alert(result);
+
+
+// Задание Js 12.
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>"); // 205
+// document.write(str - a + "<br/>") // 15
+// document.write(str * 2 + "<br/>") // 40
+// document.write(str / 2 + "<br/>") // 10
+
+
+// Задание Js 3_1.
+// let a = prompt("Введите число 11", "11");
+// if (a == 11) {
+// 	document.write("Введенное значение верно.");
+// }
+
+
+// Задание Js 3_2.
+// let userAge = prompt("Введите пожалуйста свой возраст");
+// if (userAge >= 20) {
+// 	document.write("Вы нам подходите");
+// } else {
+// 	document.write("Вы слишком молоды");
+// }
+
+
+// Задание Js 3_3.
+// let userPassword = prompt("Введите ваш пароль");
+// if (userPassword == 9583 || userPassword == 1747) {
+// 	document.write("Доступны модули баз А, B и С");
+// } else if (userPassword == 3331 || userPassword == 7922) {
+// 	document.write("Доступны модули баз В и С");
+// } else if (userPassword == 9455 || userPassword == 8997) {
+// 	document.write("Доступен модуль базы С");
+// } else if (userPassword == null) {
+// 	document.write("Отмена");
+// } else {
+// 	document.write("Отказано в доступе");
+// }
+
+
+// Задачи на основы работы с логическими операторами
+// Работа с && (и) и || (или)
+// let a = prompt("Введите значение");
+// if (a > 0 && a < 5) {
+// 	console.log("Верно");
+// } else {
+// 	console.log("Неверно");
+// }
+
+// let a = Number(prompt("Введите значение"));
+// if (a == 0 || a == 2) {
+// 	document.write(a + 7);
+// } else {
+// 	document.write(a / 10);
+// }
+
+// let a = Number(prompt("Введите значение"));
+// let b = Number(prompt("Введите значение"));
+// if (a <= 1 && b >=3) {
+// 	document.write(a + b);
+// } else {
+// 	document.write(a - b);
+// }
+
+// let a = Number(prompt("Введите значение"));
+// let b = Number(prompt("Введите значение"));
+// if (a > 2 && a < 11 || b >= 6 && b < 14) {
+// 	document.write("Верно");
+// } else {
+// 	document.write("Неверно");
+// }
+
+
+
